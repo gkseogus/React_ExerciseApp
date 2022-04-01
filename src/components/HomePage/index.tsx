9d4bfbf7680b62b859a25edc3b318f2ce85e856f
gkseogus <fbznffldj998@naver.com>
2022-04-01 17:23:43 +0900

    220401_homePage Ver.1

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Breadcrumb, Layout } from 'antd';
 
-const index = () => {
+const { Content } = Layout;
+
+const HomePage = () => {
   return (
     <div>
-      
+      <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        운동하죠
+      </Breadcrumb>
+      <div className="site-layout-content">그러죠 </div>
+    </Content>
     </div>
   );
 };
 
-export default index;
+export default React.memo(HomePage);
