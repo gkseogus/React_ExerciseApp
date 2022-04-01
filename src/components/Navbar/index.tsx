053cb10846fbf3e0885e5f39fd425dafc8469f93
gkseogus <fbznffldj998@naver.com>
2022-04-01 17:23:58 +0900

    220401_navBar ver.1

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Menu } from 'antd';
 
-const index = () => {
+
+const { Header, Footer } = Layout;
+
+const Navbar = () => {
+
+
   return (
-    <div>
-      
-    </div>
+    <Layout className="layout">
+    <Header style={{ 
+      position: 'fixed', 
+      zIndex: 1, 
+      width: '100%',
+      background:'black' ,
+      }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}
+        style={{
+          background: 'black',
+        }}
+      >
+        <Menu.Item key="1"
+        style={{
+
+        }}      
+        >Home</Menu.Item>
+        <Menu.Item key="2"
+        style={{
+
+        }}      
+        >종목</Menu.Item>
+        <Menu.Item key="3"
+        style={{
+
+        }}     
+        >루틴</Menu.Item>
+      </Menu>
+    </Header>
+    <Footer style={{ textAlign: 'center' }}>운동 사이트</Footer>
+  </Layout>
   );
 };
 
-export default index;
+export default React.memo(Navbar);
