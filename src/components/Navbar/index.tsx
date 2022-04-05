1e1386cc73b4702b6dfec07785de097214cd490d
gkseogus <fbznffldj998@naver.com>
2022-04-06 08:57:18 +0900

    22040^_navbar font 업데이트

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Layout, Menu } from 'antd';
+import styled from 'styled-components';
 
+const MenuFont = styled.h1`
+ font-family: 'Rubik Bubbles', cursive;
+ padding: -5px;
+`
 
 const { Header, Footer } = Layout;
 
@@ -9,34 +14,30 @@ const Navbar = () => {
 
 
   return (
-    <Layout className="layout">
+    <Layout className='layout'>
     <Header style={{ 
       position: 'fixed', 
       zIndex: 1, 
       width: '100%',
-      background:'linear-gradient(to right top, #94eb09, #000000)' ,
+      background:'linear-gradient(to right top, #94eb09, #000000)',
+      height: '74.5px'
       }}>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}
+      <div className='logo' />
+      <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}
         style={{
           background: 'linear-gradient(to right top, #94eb09, #000000)',
         }}
       >
-        <Menu.Item key="1"
-        style={{
-
-        }}      
-        >Home</Menu.Item>
-        <Menu.Item key="2"
-        style={{
-
-        }}      
-        >종목</Menu.Item>
-        <Menu.Item key="3"
-        style={{
-
-        }}     
-        >루틴</Menu.Item>
+        <Menu.Item key='1'>
+          <MenuFont>
+            Home
+          </MenuFont>
+        </Menu.Item>
+        <Menu.Item key='2'>
+          <MenuFont>
+            Routine
+          </MenuFont>
+        </Menu.Item>
       </Menu>
     </Header>
     <Footer style={{ textAlign: 'center' }}>운동 사이트</Footer>
