43697637695422db04fd41e726719ab4044f3c38
gkseogus <fbznffldj998@naver.com>
2022-04-08 12:22:08 +0900

    220408_protein page

---
+++ src/components/ProteinPage/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Space } from 'antd';
+import styled from 'styled-components';
+import Proteins from '../Proteins';
+
+
+const Constain = styled.div`
+  padding: 1em;
+  background: black;
+  height: 100vh;
+`
+
+const FirstFont = styled.h1`
+  position: static;
+  font-size: 100px;
+  text-align: center;
+  background: linear-gradient(to right top, #000000, #94eb09);
+  color: transparent;
+  -webkit-background-clip: text;
+  font-family: 'Rubik Glitch', cursive;
+`
+
+const ProteinForm = styled.div`
+    display: flex;
+    justify-content: center;
+`
+const { Search } = Input;
+
+const ProteinPage = () => {
+  const [name, setName] = useState('');
+
+  return (
+    <Constain>  
+        <FirstFont>
+            Protein 
+        </FirstFont>
+        <ProteinForm>
+            <Space direction="vertical">
+                <Search placeholder="search protein" allowClear onSearch={setName} style={{ 
+                    width: 400,
+                    padding: '20px'
+                    }} />
+            </Space>
+        </ProteinForm>
+        <Proteins/>
+    </Constain>
+  );
+};
+
+export default React.memo(ProteinPage);
