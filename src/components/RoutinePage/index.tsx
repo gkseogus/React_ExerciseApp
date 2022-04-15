8d47404f966f272af58686788fa6ce3414cce466
gkseogus <fbznffldj998@naver.com>
2022-04-15 17:35:22 +0900

    220415_routine update

--- src/components/RoutinePage/index.tsx
+++ src/components/RoutinePage/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import styled from 'styled-components';
-import RoutineItems from '../RoutineItems';
-import { Nav } from 'react-bootstrap';
+import { Tabs,Tab } from 'react-bootstrap';
+import Chest from '../RoutineItems/chest';
+import Back from '../RoutineItems/back';
+import Arm from '../RoutineItems/arm';
+import Shulder from '../RoutineItems/shoulder';
 
 const Constain = styled.div`
   padding: 1em;
   background: black;
+  height: 100vh;
 `
 
 const FirstFont = styled.h1`
@@ -18,9 +22,23 @@ const FirstFont = styled.h1`
   -webkit-background-clip: text;
   font-family: 'Rubik Glitch', cursive;
 `
+const ContentsFont = styled.h1`
+  padding: 5%;
+  font-size: 20px;
+  text-align: center;
+  background: linear-gradient(to right top, #000000, #94eb09);
+  color: transparent;
+  -webkit-background-clip: text;
+  font-family: 'Permanent Marker', cursive;
+`
+const TabFont = styled.h3`
+  font-size: 20px;
+  text-align: center;
+  color: white;
+  font-family: 'Black Han Sans', sans-serif;
+`
 
-const NavFont = styled.h3`
-  font-size: 30px;
+const TabsFont = styled.h3`
   background: linear-gradient(to right top, #000000, #94eb09);
   color: transparent;
   -webkit-background-clip: text;
@@ -32,32 +50,44 @@ const RoutinePage = () => {
   return (
     <Constain>  
       <FirstFont>
-          Routine
+        Routine
       </FirstFont>
-      <Nav justify variant="tabs" defaultActiveKey="/home" >
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">
-            <NavFont>
-              Loooonger NavLink
-            </NavFont>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">              
-            <NavFont>
-              Loooonger NavLink
-            </NavFont>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-3">              
-            <NavFont>
-              Loooonger NavLink
-            </NavFont>
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <RoutineItems/>
+      <TabsFont>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3" mountOnEnter={true}>
+          <Tab eventKey="1" title="Chest">
+            <ContentsFont>
+              Chest Exercise Routine
+            </ContentsFont>
+            <TabFont>저중량: 5 set, 8회~ 12회</TabFont>
+            <TabFont>고중량: 5 set, 5회 ~ 10회</TabFont>
+            <Chest/>
+          </Tab>
+          <Tab eventKey="2" title="Back">
+            <ContentsFont>
+              Back Exercise Routine
+            </ContentsFont>
+            <TabFont>저중량: 5 set, 8회~ 12회</TabFont>
+            <TabFont>고중량: 5 set, 5회 ~ 10회</TabFont>
+            <Back/>
+          </Tab>
+          <Tab eventKey="3" title="Arm">
+            <ContentsFont>
+              Arm Exercise Routine
+            </ContentsFont>
+            <TabFont>저중량: 5 set, 8회~ 12회</TabFont>
+            <TabFont>고중량: 5 set, 5회 ~ 10회</TabFont>
+            <Arm/>
+          </Tab>
+          <Tab eventKey="4" title="Shoulder">
+            <ContentsFont>
+              Shoulder Exercise Routine
+            </ContentsFont>
+            <TabFont>저중량: 5 set, 8회~ 12회</TabFont>
+            <TabFont>고중량: 5 set, 5회 ~ 10회</TabFont>
+            <Shulder/>
+          </Tab>
+        </Tabs>
+      </TabsFont>
     </Constain>
   );
 };
