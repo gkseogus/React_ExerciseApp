2a3a2d8c0544be06c59f60011ad7efc453dec48a
gkseogus <fbznffldj998@naver.com>
2022-04-15 16:43:10 +0900

    220415_add to nav

--- src/components/RoutinePage/index.tsx
+++ src/components/RoutinePage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import styled from 'styled-components';
 import RoutineItems from '../RoutineItems';
+import { Nav } from 'react-bootstrap';
 
 const Constain = styled.div`
   padding: 1em;
@@ -18,6 +19,14 @@ const FirstFont = styled.h1`
   font-family: 'Rubik Glitch', cursive;
 `
 
+const NavFont = styled.h3`
+  font-size: 30px;
+  background: linear-gradient(to right top, #000000, #94eb09);
+  color: transparent;
+  -webkit-background-clip: text;
+  font-family: 'Permanent Marker', cursive;
+`
+
 const RoutinePage = () => {
 
   return (
@@ -25,6 +34,29 @@ const RoutinePage = () => {
       <FirstFont>
           Routine
       </FirstFont>
+      <Nav justify variant="tabs" defaultActiveKey="/home" >
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">
+            <NavFont>
+              Loooonger NavLink
+            </NavFont>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">              
+            <NavFont>
+              Loooonger NavLink
+            </NavFont>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">              
+            <NavFont>
+              Loooonger NavLink
+            </NavFont>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
       <RoutineItems/>
     </Constain>
   );
