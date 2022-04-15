93f6ea6ca02035fb31cd8103dc5bb7ac9a339120
gkseogus <fbznffldj998@naver.com>
2022-04-15 16:42:49 +0900

    220415_change font style

--- src/components/RoutineItems/index.tsx
+++ src/components/RoutineItems/index.tsx
@@ -3,14 +3,24 @@ import styled from 'styled-components';
 import { Carousel } from 'react-bootstrap';
 
 const RoutineFout = styled.h2`
+  font-family: 'Black Han Sans', sans-serif;
   text-align: center;
   color: white;
   padding: 40px;
 `
 
 const ListBox = styled.div`
-    padding: 80px;
+    padding: 10%;
     height: 200px;
+`
+
+const ContentsFont = styled.h1`
+  font-size: 20px;
+  text-align: center;
+  background: linear-gradient(to right top, #000000, #94eb09);
+  color: transparent;
+  -webkit-background-clip: text;
+  font-family: 'Permanent Marker', cursive;
 `
 
 const RoutineItem = () => {
@@ -18,90 +28,102 @@ const RoutineItem = () => {
     return (
         <div>
             <ListBox>
+                <ContentsFont>
+                    dwdwf
+                </ContentsFont>
                 <Carousel>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>덤벨 프레스</RoutineFout>
+                        <RoutineFout>덤벨 프레스</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>벤치 프레스</RoutineFout>
+                        <RoutineFout>벤치 프레스</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>해머 프레스</RoutineFout>
+                        <RoutineFout>해머 프레스</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>인클라인 머신</RoutineFout>
+                        <RoutineFout>인클라인 머신</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>체스트 플라이 머신</RoutineFout>
+                        <RoutineFout>체스트 플라이 머신</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>스미스 벤치 머신</RoutineFout>
+                        <RoutineFout>스미스 벤치 머신</RoutineFout>
                     </Carousel.Item>
                 </Carousel>
             </ListBox>
             <ListBox>
+                <ContentsFont>
+                    dwdwf
+                </ContentsFont>
                 <Carousel>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>루마니안 데드</RoutineFout>
+                        <RoutineFout>루마니안 데드</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>바벨 로우</RoutineFout>
+                        <RoutineFout>바벨 로우</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>덤벨 로우</RoutineFout>
+                        <RoutineFout>덤벨 로우</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>미드 로우</RoutineFout>
+                        <RoutineFout>미드 로우</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>케이블 로우</RoutineFout>
+                        <RoutineFout>케이블 로우</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>T바 로우</RoutineFout>
+                        <RoutineFout>T바 로우</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>렛풀 다운</RoutineFout>
+                        <RoutineFout>렛풀 다운</RoutineFout>
                     </Carousel.Item>
                 </Carousel>
             </ListBox>
             <ListBox>
+                <ContentsFont>
+                    dwdwf
+                </ContentsFont>
                 <Carousel>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>사이드 레터럴 레이즈</RoutineFout>
+                        <RoutineFout>사이드 레터럴 레이즈</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>프론트 레터럴 레이즈</RoutineFout>
+                        <RoutineFout>프론트 레터럴 레이즈</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>덤벨 숄더 프레스</RoutineFout>
+                        <RoutineFout>덤벨 숄더 프레스</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>밀리터리 프레스</RoutineFout>
+                        <RoutineFout>밀리터리 프레스</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>벤트 오버 레터럴 레이즈</RoutineFout>
+                        <RoutineFout>벤트 오버 레터럴 레이즈</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>펙덱 플라이</RoutineFout>
+                        <RoutineFout>펙덱 플라이</RoutineFout>
                     </Carousel.Item>
                 </Carousel>
             </ListBox>
-            <ListBox>
+            <ListBox>   
+                <ContentsFont>
+                    dwdwf
+                </ContentsFont>
                 <Carousel>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>덤벨 컬</RoutineFout>
+                        <RoutineFout>덤벨 컬</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>바벨 컬</RoutineFout>
+                        <RoutineFout>바벨 컬</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>케이블 푸쉬 다운</RoutineFout>
+                        <RoutineFout>케이블 푸쉬 다운</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>네이즐 트라이 셉</RoutineFout>
+                        <RoutineFout>네이즐 트라이 셉</RoutineFout>
                     </Carousel.Item>
                     <Carousel.Item interval={2000}>
-                            <RoutineFout>스탠딩 로프 트라이 셉 익스텐션</RoutineFout>
+                        <RoutineFout>스탠딩 로프 트라이 셉 익스텐션</RoutineFout>
                     </Carousel.Item>
                 </Carousel>
             </ListBox>
