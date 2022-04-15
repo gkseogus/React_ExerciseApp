696105f309a6aad3e466c4bbcedba4b77ca0d6f0
gkseogus <fbznffldj998@naver.com>
2022-04-15 15:01:32 +0900

    220415_routinePage

---
+++ src/components/RoutinePage/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import styled from 'styled-components';
+
+
+const Constain = styled.div`
+  padding: 1em;
+  background: black;
+  height: 100vh;
+`
+
+const FirstFont = styled.h1`
+  position: static;
+  font-size: 100px;
+  text-align: center;
+  background: linear-gradient(to right top, #000000, #94eb09);
+  color: transparent;
+  -webkit-background-clip: text;
+  font-family: 'Rubik Glitch', cursive;
+`
+
+const RoutinePage = () => {
+
+  return (
+    <Constain>  
+        <FirstFont>
+            Routine
+        </FirstFont>
+    </Constain>
+  );
+};
+
+export default React.memo(RoutinePage);
