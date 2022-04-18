1bba0526734a412c7093c5d610953bf86b44ef0e
gkseogus <fbznffldj998@naver.com>
2022-04-18 16:26:24 +0900

    220418_api 구축 중

--- src/components/GoogleSheet/index.tsx
+++ src/components/GoogleSheet/index.tsx
@@ -1,25 +1,34 @@
+import { useDispatch } from "react-redux";
 import { fetchRequest } from "../../store/inventory/action";
 
 declare const window: Window & {
     gapi: any;
   };
  
-  
+const dispatch = useDispatch();  
+
 export const loadTodaySheet = async () => {
+
     window.gapi.client.sheets.spreadsheets.values
     .get({
       spreadsheetId: '1IRgIFpdzgBnPbWOKDQyTYndhRsxQMKYdfH989n6fRQQ',
-      range: 'A1:E6',
+      range: 'A2:E6',
     })
-    .then((response: any) => {
-      fetchRequest(
-        response.result.values.map((row: string[]) => ({
-          name: row[0],
-          price: parseInt(row[1]),
-          flatness: row[2],
-          brand: row[3]
-        }))
-      );
+    .then((res: any) => {
+      if(res.status === 200){
+        res.json().then((data: any) => {
+          const apiData = data
+          dispatch(fetchRequest(
+            apiData.map((row: string[]) => ({
+              name: row[0],
+              price: parseInt(row[1]),
+              flatness: row[2],
+              brand: row[3],
+              image: row[4]
+            }))
+          ));
+        })
+      }
       console.log('test')
     });
 }
