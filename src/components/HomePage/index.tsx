7fa63a619eb7cd836e09db1b7fafea64c69ecebd
gkseogus <fbznffldj998@naver.com>
2022-04-18 17:09:38 +0900

    220418_add to Footer

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Cards from '../Cards';
+import { Card } from 'react-bootstrap';
 
 const Constain = styled.div`
   padding: 1em;
@@ -25,17 +26,35 @@ const FirstFont = styled.h1`
   font-family: 'Rubik Glitch', cursive;
 `
 
+const FooterFont = styled.h6`
+  text-align: center;
+`
+
 const HomePage = () => {
 
   return (
-    <Constain>  
-      <FirstFont>
-        Exercise Blog
-      </FirstFont>
-      <ListConstain>
-        <Cards/>
-      </ListConstain>
-    </Constain>
+    <div>
+      <Constain>  
+        <FirstFont>
+          Exercise Blog
+        </FirstFont>
+        <ListConstain>
+          <Cards/>
+        </ListConstain>
+      </Constain>
+      <Card>
+        <FooterFont>
+          <Card.Header as="h6">단순 운동 취미를 가진 개발자 사이트 입니다.</Card.Header>
+        </FooterFont>
+        <Card.Body>
+          <Card.Title>-사이트 정보-</Card.Title>
+          <Card.Text>
+            개발자 연락처: 010-2246-6787 <br/>
+            개발자 이메일: fbzbffldj@gmail.com
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
