d2694d32c7c28992241914437bf64a1fc075a6b2
gkseogus <fbznffldj998@naver.com>
2022-04-18 15:39:32 +0900

    220418_Protein 페이지 수정

--- src/components/ProteinPage/index.tsx
+++ src/components/ProteinPage/index.tsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
-import { Input, Space } from 'antd';
 import styled from 'styled-components';
 import ProteinItems from '../ProteinItems';
-import { ApplicationState } from '../../store';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRequest } from '../../store/inventory/action';
 
 const Constain = styled.div`
   padding: 1em;
@@ -23,41 +19,13 @@ const FirstFont = styled.h1`
   font-family: 'Rubik Glitch', cursive;
 `
 
-const ProteinForm = styled.div`
-    display: flex;
-    justify-content: center;
-`
-const { Search } = Input;
-
 const ProteinPage = (itme: any) => {
-  const [name, setName] = useState('');
-  const dispatch = useDispatch();
-
-  const rootData = useSelector(
-    (state: ApplicationState) => state.inventory.data
-  );
-
-  const data = (rootData).filter(
-    (i) => !name || i.name.includes(name)
-  );
-  
-  // if(data !== null && data !== undefined){
-  //   dispatch(fetchRequest(data));
-  // }
 
   return (
     <Constain>  
         <FirstFont>
             Protein 
         </FirstFont>
-        <ProteinForm>
-            <Space direction="vertical">
-                <Search placeholder="search protein" allowClear onSearch={setName} style={{ 
-                    width: 400,
-                    padding: '20px'
-                    }} />
-            </Space>
-        </ProteinForm>
         <ProteinItems/>
     </Constain>
   );
