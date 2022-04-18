1bdeda7314ecb0e87aa9383f45fbcc4e99251c72
gkseogus <fbznffldj998@naver.com>
2022-04-18 15:47:49 +0900

    220418_검색기능 추가

--- src/components/ProteinItems/index.tsx
+++ src/components/ProteinItems/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Figure } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { ApplicationState } from '../../store';
 import { v4 as uuid_v4 } from 'uuid';
+import 'antd/dist/antd.css';
+import { Input, Space } from 'antd';
 
 const Constain = styled.div`
     display: flex;
@@ -14,7 +16,7 @@ const ListConstain = styled.li`
   position: static;
   margin-left: auto;
   margin-right: auto;
-  padding: 10px;
+  padding: 100px;
 `
 
 const ExplainFont = styled.h6`
@@ -22,22 +24,46 @@ const ExplainFont = styled.h6`
     text-align: center;
 `
 
+const ProteinSearchForm = styled.div`
+    width:300px;
+    height:300px;
+    position:absolute;
+    left:50%;
+    top:50%;
+    margin-left:-200px;
+    margin-top:-150px;
+`
+
 const ProteinItems = () => {
-      
+    const { Search } = Input;
+
+    const [searchKeyword, setSearchKeyword] = useState('');
+
     const rootData = useSelector(
         (state: ApplicationState) => state.inventory.data
     );
     
-    console.log('Item List',rootData);
-      
+    const searchData =  rootData.filter(
+      (i) => !searchKeyword || i.name.includes(searchKeyword)
+    );
+  
     return (
     <Constain>
+        <ProteinSearchForm>
+            <Space direction="vertical">
+                <Search placeholder="search protein" allowClear onChange={(e) => setSearchKeyword(e.target.value)}
+                style={{ 
+                    width: 400,
+                    padding: '20px'
+                }} />
+            </Space>
+        </ProteinSearchForm>
         <ListConstain>
             <Figure style={{ padding: '10px'}}>
                 <Figure.Caption>
                     <ExplainFont>
                         {
-                            rootData.map(item => {
+                            searchData.map(item => {
                                 return(
                                     <div key={uuid_v4()}>
                                         <Figure.Image
@@ -45,11 +71,11 @@ const ProteinItems = () => {
                                             height={200}
                                             alt="200x200"
                                             src={item.image}
-                                        /><br/>
+                                        /><br/><br/>
                                         이름: {item.name} <br/>
                                         브랜드: {item.brand} <br/>
                                         가격: {item.price} <br/>
-                                        맛: {item.flatness} <br/>
+                                        맛: {item.flatness} <br/><br/>
                                     </div>
                                 );
                             })
