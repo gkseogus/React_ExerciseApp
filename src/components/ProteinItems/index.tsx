c8edf7170e4eece769a4089b1a20047cb311fb85
gkseogus <fbznffldj998@naver.com>
2022-04-20 10:12:10 +0900

    220420_사설 api 삭제

--- src/components/ProteinItems/index.tsx
+++ src/components/ProteinItems/index.tsx
@@ -32,80 +32,57 @@ const ProteinSearchForm = styled.div`
 `
 
 const ProteinItems = () => {
-    const { Search } = Input;
+  const { Search } = Input;
 
-    const dispatch = useDispatch();
-    const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
 
-    const rootData = useSelector(
-        (state: ApplicationState) => state.inventory.data
-    );
-    
-    const searchData =  rootData.filter(
-      (i) => !searchKeyword || i.name.includes(searchKeyword)
-    );
-
-    const getData = async () => {
-        try {
-          await trackPromise(fetch(
-            `https://api.apispreadsheets.com/data/FJTALrTYaT8MLcVS/`
-            ).then(res=>{
-              if(res.status === 200){
-                res.json().then(data=>{
-                  const apiData = data
-                  dispatch(fetchRequest(apiData.data));
-                }).catch(err => console.log(err))
-              }
-            }))
-        } catch (err) {
-          console.log('error:', err);
-        }
-      };
-    
-    
-      useEffect(() => {
-        getData();
-      });
-    
-    return (
-    <div>
-        <ProteinSearchForm>
-            <Space direction="vertical">
-                <Search placeholder="search protein" allowClear={true} onChange={(e) => setSearchKeyword(e.target.value)}
-                style={{ 
-                    width: 400,
-                    padding: '20px'
-                }} />
-            </Space>
-        </ProteinSearchForm>
-        <ListConstain>
-            <Figure style={{ padding: '10px'}}>
-                <Figure.Caption>
-                    <ExplainFont>
-                        {
-                            searchData.map(item => {
-                                return(
-                                    <div key={uuid_v4()}>
-                                        <Figure.Image
-                                            width={200}
-                                            height={200}
-                                            alt="200x200"
-                                            src={item.image}
-                                        /><br/><br/>
-                                        이름: {item.name} <br/>
-                                        브랜드: {item.brand} <br/>
-                                        가격: {item.price} <br/>
-                                        맛: {item.flatness} <br/><br/>
-                                    </div>
-                                );
-                            })
-                        }
-                    </ExplainFont>
-                </Figure.Caption>
-            </Figure>
-        </ListConstain>
-    </div>
-    );
+  const rootData = useSelector(
+      (state: ApplicationState) => state.inventory.data
+  );
+  
+  const searchData =  rootData.filter(
+    (i) => !searchKeyword || i.name.includes(searchKeyword)
+  );
+  
+  return (
+  <div>
+      <ProteinSearchForm>
+          <Space direction="vertical">
+              <Search placeholder="search protein" allowClear={true} onChange={(e) => setSearchKeyword(e.target.value)}
+              style={{ 
+                  width: 400,
+                  padding: '20px'
+              }} />
+          </Space>
+      </ProteinSearchForm>
+      <ListConstain>
+          <Figure style={{ padding: '10px'}}>
+              <Figure.Caption>
+                  <ExplainFont>
+                      {
+                          searchData.map(item => {
+                              return(
+                                  <div key={uuid_v4()}>
+                                      <Figure.Image
+                                          width={200}
+                                          height={200}
+                                          alt="200x200"
+                                          src={item.image}
+                                      /><br/><br/>
+                                      이름: {item.name} <br/>
+                                      브랜드: {item.brand} <br/>
+                                      가격: {item.price} <br/>
+                                      맛: {item.flatness} <br/><br/>
+                                  </div>
+                              );
+                          })
+                      }
+                  </ExplainFont>
+              </Figure.Caption>
+          </Figure>
+      </ListConstain>
+  </div>
+  );
 };
 
 export default React.memo(ProteinItems);
