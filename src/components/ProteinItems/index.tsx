3fbc05c059614d8b3df9b04d17ac542c81877499
gkseogus <fbznffldj998@naver.com>
2022-04-20 10:30:15 +0900

    220420_들여쓰기 줄임

--- src/components/ProteinItems/index.tsx
+++ src/components/ProteinItems/index.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Figure } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { ApplicationState } from '../../store';
 import { v4 as uuid_v4 } from 'uuid';
 import 'antd/dist/antd.css';
 import { Input, Space } from 'antd';
-import { fetchRequest } from '../../store/inventory/action';
-import { trackPromise } from 'react-promise-tracker';
 
 const ListConstain = styled.li`
   text-align: center;
@@ -46,41 +44,41 @@ const ProteinItems = () => {
   
   return (
   <div>
-      <ProteinSearchForm>
-          <Space direction="vertical">
-              <Search placeholder="search protein" allowClear={true} onChange={(e) => setSearchKeyword(e.target.value)}
-              style={{ 
-                  width: 400,
-                  padding: '20px'
-              }} />
-          </Space>
-      </ProteinSearchForm>
-      <ListConstain>
-          <Figure style={{ padding: '10px'}}>
-              <Figure.Caption>
-                  <ExplainFont>
-                      {
-                          searchData.map(item => {
-                              return(
-                                  <div key={uuid_v4()}>
-                                      <Figure.Image
-                                          width={200}
-                                          height={200}
-                                          alt="200x200"
-                                          src={item.image}
-                                      /><br/><br/>
-                                      이름: {item.name} <br/>
-                                      브랜드: {item.brand} <br/>
-                                      가격: {item.price} <br/>
-                                      맛: {item.flatness} <br/><br/>
-                                  </div>
-                              );
-                          })
-                      }
-                  </ExplainFont>
-              </Figure.Caption>
-          </Figure>
-      </ListConstain>
+    <ProteinSearchForm>
+      <Space direction="vertical">
+        <Search placeholder="search protein" allowClear={true} onChange={(e) => setSearchKeyword(e.target.value)}
+        style={{ 
+          width: 400,
+          padding: '20px'
+        }} />
+      </Space>
+    </ProteinSearchForm>
+    <ListConstain>
+      <Figure style={{ padding: '10px'}}>
+        <Figure.Caption>
+          <ExplainFont>
+            {
+              searchData.map(item => {
+                return(
+                  <div key={uuid_v4()}>
+                    <Figure.Image
+                      width={200}
+                      height={200}
+                      alt="200x200"
+                      src={item.image}
+                    /><br/><br/>
+                    이름: {item.name} <br/>
+                    브랜드: {item.brand} <br/>
+                    가격: {item.price} <br/>
+                    맛: {item.flatness} <br/><br/>
+                  </div>
+                );
+              })
+            }
+          </ExplainFont>
+        </Figure.Caption>
+      </Figure>
+    </ListConstain>
   </div>
   );
 };
