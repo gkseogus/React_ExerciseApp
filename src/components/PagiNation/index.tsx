19b110f46d9019156157ac1a19115816469f36c4
gkseogus <fbznffldj998@naver.com>
2022-04-27 11:39:06 +0900

    ...

--- src/components/PagiNation/index.tsx
+++ src/components/PagiNation/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { ApplicationState } from '../../store';
 
 const PagiNation = () => {
   const items = [];
