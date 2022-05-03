400001a60ae941e37ba542b5984c826b1fec070e
gkseogus <fbznffldj998@naver.com>
2022-05-03 15:14:42 +0900

    220503_add to notice

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Navbar from './components/NavBar';
+import NoticePage from './components/NoticePage';
 import ProteinPage from './components/ProteinPage';
 import RoutinePage from './components/RoutinePage';
 import WeightPage from './components/WeightPage';
@@ -41,6 +42,15 @@ const RoutesApp = () => (
         <div>
           <Navbar/>
           <ProteinPage/>
+        </div>
+      }
+    />
+    <Route  
+      path='/notice'
+      element={
+        <div>
+          <Navbar/>
+          <NoticePage/>
         </div>
       }
     />
