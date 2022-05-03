0466362f231eed7472b077e95b55e500d83f3575
gkseogus <fbznffldj998@naver.com>
2022-05-03 16:49:47 +0900

    220503_add to notice data

---
+++ src/components/NoticePage/noticeData.js
@@ -0,0 +1,49 @@
+export const noticeData = [
+ {
+  key: '1',
+  title: '2022/04/01__ver.1',
+  text: 'MainPage 및 NavBar 설정 ',
+ },
+ {
+  key: '2',
+  title: '2022/04/06__ver.2',
+  text: 'logo icon 추가 , google font 적용 , use react boot strap ',
+ },
+ {
+  key: '3',
+  title: '2022/04/07__ver.3',
+  text: '메뉴 카드 삽입',
+ },
+ {
+  key: '4',
+  title: '2022/04/08__ver.4',
+  text:
+   '프로틴 페이지 생성 , 프로틴 페이지의 데이터 바인딩 , navbar 링크 삽입 ',
+ },
+ {
+  key: '5',
+  title: '2022/04/12__ver.5',
+  text: 'route 연결',
+ },
+ {
+  key: '6',
+  title: '2022/04/13__ver.6',
+  text: '프로틴 페이지 디자인 수정 , 데이터 바인딩 수정 , api 호출 ',
+ },
+ {
+  key: '7',
+  title: '2022/04/14__ver.6',
+  text: '검색기능 추가 및 테스트',
+ },
+ {
+  key: '8',
+  title: '2022/04/15__ver.7',
+  text:
+   '루틴 페이지 추가 및 디자인 삽입 , 루틴 페이지의 데이터 삽입 , carousels 적용',
+ },
+ {
+  key: '9',
+  title: '2022/04/18__ver.8',
+  text: '검색기능 개편 , footer 추가 , 각종 디자인 수정 , api 호출 개편 ',
+ },
+];
