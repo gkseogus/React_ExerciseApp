fea6f5180c25773e5fe61f8064b654dd98ca2835
gkseogus <fbznffldj998@naver.com>
2022-05-09 14:58:55 +0900

    220509_add to notice data

--- src/components/NoticePage/noticeData.js
+++ src/components/NoticePage/noticeData.js
@@ -51,4 +51,39 @@ export const noticeData = [
   title: '2022/04/18__ver.9',
   text: '시트 api 생성, 로딩 표기 기능, trackPromise 추가 ',
  },
+ {
+  key: '11',
+  title: '2022/04/19__ver.10',
+  text: '시트 api 업데이트, 로딩 표시 업데이트 ',
+ },
+ {
+  key: '12',
+  title: '2022/04/21__ver.11',
+  text: 'radar chart 추가',
+ },
+ {
+  key: '13',
+  title: '2022/04/25__ver.12',
+  text: '웹 반응형 디자인 적용, footer 추가, BGM 링크 설정, 인바디 데이터 추가',
+ },
+ {
+  key: '14',
+  title: '2022/04/26__ver.13',
+  text: '페이지 네이션 디자인 추가',
+ },
+ {
+  key: '15',
+  title: '2022/04/29__ver.14',
+  text: 'BROWSER ROUTER 추가, route 변경',
+ },
+ {
+  key: '16',
+  title: '2022/05/03__ver.15',
+  text: '공지사항 데이터 바인딩, 공지사항 페이지 생성',
+ },
+ {
+  key: '17',
+  title: '2022/05/04__ver.16',
+  text: '호버 이벤트 추가, 로그인 버튼 디자인 변경',
+ },
 ];
