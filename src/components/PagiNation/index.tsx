e0fbbdb1583e6ab9cafe31a90d52a7294f4bd003
gkseogus <fbznffldj998@naver.com>
2022-06-16 12:08:14 +0900

    220616_pagination

--- src/components/PagiNation/index.tsx
+++ src/components/PagiNation/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Pagination } from 'antd';
 import styled from 'styled-components';
+import { ApplicationState } from '../../store';
+import { useSelector } from 'react-redux';
 
 const Constain = styled.div`
   color: white;
@@ -9,9 +11,19 @@ const Constain = styled.div`
 
 const PagiNation = () => {
 
+  const rootData = useSelector(
+    (state: ApplicationState) => state.inventory.data
+  );
+
+  const onChange = () => {
+    for(let i=0; i<rootData.length; i++){
+      return rootData[i]
+    }
+  }
+
   return (
     <Constain>
-      <Pagination defaultCurrent={1} total={50} responsive={true} />
+      <Pagination defaultCurrent={1} total={50} responsive={true} defaultPageSize={5} onChange={onChange} pageSize={5} />
     </Constain>
   );
 };
